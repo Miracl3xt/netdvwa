81c80adf212128f9e0705607d4a0575d4fc9c6d0
Miracle <44254112+Miracl3xt@users.noreply.github.com>
2024-05-30 16:26:42 +0530

    Update attacks.py

--- dsvpwa/attacks.py
+++ dsvpwa/attacks.py
@@ -9,7 +9,7 @@ import subprocess
 import urllib.request
 import urllib.parse as urlparse
 
-#123dgfdggvbcvbvcb
+#123dgfdggvbcvbvcbddfdf
 class Attack():
     warning = (
         'This attack vector is unavailable on the current risk level ({}). '
